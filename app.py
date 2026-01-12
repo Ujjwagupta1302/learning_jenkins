256f8812d30cf1d7acd7988bac689ff8f691e7e9
Ujjwal Gupta <150505461+Ujjwagupta1302@users.noreply.github.com>
2026-01-12 18:41:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello! If you see this, Jenkins successfully built and deployed this container. It is after edit!"
+    return "Hello! If you see this, Jenkins successfully built and deployed this container. It is after edit2!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
